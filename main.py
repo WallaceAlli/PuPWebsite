c6a58e139e83ab037b79c1183fcd136f2b4d8ee2
WallaceAlli <alliwaliy12@gmail.com>
2023-10-15 23:49:55 -0500

    Update on 10/15/2023 11:50 Root to IP

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm,LoginForm
 from flask_mysqldb import MySQL
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'd8c3f8a28b8b162bbee3fca47e8e0b74'
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1' #Change this to Tylers IP address of his SQL
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Ideapad320@'
 app.config['MYSQL_DB'] = 'capstone'
 
 mysql = MySQL()
 mysql.init_app(app)
-LoggedIn = False
-
 
 
 @app.route("/login", methods=['GET','POST'])
